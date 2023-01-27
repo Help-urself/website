6be85388d346b306e54c59ecc47e951960b4f8ad
Alex Stepens <107001315+Help-urself@users.noreply.github.com>
2023-01-27 21:47:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import flask 
 from flask import Flask,render_template
 import os
-app = Flask(__name__, template_folder='website/templates/', 
+app = Flask(__name__, template_folder='/website/templates/', 
 static_folder='website/static/')
 
 
 @app.route('/')
 def t():
-    return render_template("website.html")
+    return render_template("web.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
