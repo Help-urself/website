29c1da160c91c93dc17c0bd737e3e089e4d28962
Alex Stepens <107001315+Help-urself@users.noreply.github.com>
2023-01-27 21:38:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+import flask 
+from flask import Flask,render_template
 import os
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/templates/', 
+static_folder='/static/')
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+def t():
+    return render_template("website.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
