f959b80c0a2d6421afbab7a2b193b987fdd7d9d2
Alex Stepens <107001315+Help-urself@users.noreply.github.com>
2023-01-28 12:30:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import os
 app = Flask(__name__, template_folder='templates', 
 static_folder='static')
 
-
 @app.route('/')
 def t():
-    return render_template("web.html")
+    return render_template("start.html")
+
+
+@app.route('/PC')
+def ht():
+    return render_template("website.html")
+
+@app.route('/telephone')
+def htt():
+    return render_template("website_telephon.html")
 
 #не менять
 if __name__ == '__main__':
